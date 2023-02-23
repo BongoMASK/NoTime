9c074894d084dee9312eec33a2baf58d8fc0505e
DEEP-32 <anshu3012ep@gmail.com>
2023-02-23 13:29:18 +0530

    Added jump, air movements and falling gravity

--- Assets/In Game/Scripts/FPSController.cs
+++ Assets/In Game/Scripts/FPSController.cs
@@ -3,59 +3,30 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class FPSController : MonoBehaviour
 {
+    #region functional Toggles
     [Header("Functional toggle"), Tooltip("To on and off some player functionality")]
     [SerializeField] private bool isActive = true;
     [SerializeField] private bool canWalk = true;
     [SerializeField] private bool canSprint = true;
     [SerializeField] private bool canInteract = true;
+    [SerializeField] private bool canAirMove = true;
+    [SerializeField] private bool canJump = true;
+    #endregion
 
-    //References.
+    #region Internal references
     private Rigidbody rb;
+    private CapsuleCollider capCollider;
+    #endregion
 
-
-
-
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        cam = GetComponentInChildren<Camera>();
-        rb.freezeRotation = true;
-    }
-
-    private void Start()
-    {
-
-    }
-
-
-    private void Update()
-    {
-        if (!isActive)
-            return;
-
-        TakeInput();
-        RotatePlayer();
-        SpeedControl();
-        AccelerateSpeed();
-        Debug.Log(rb.velocity);
-
-
-    }
-
-    private void FixedUpdate()
-    {
-        if(canWalk || canSprint)
-           Move();
-    }
-
-
+    #region Basic Control,Movement,Inputs
 
     [Header("MOVE")]
     [SerializeField] private float walkSpeed = 4f;
     [SerializeField] private float sprintSpeed = 7f;
     [SerializeField] private float acceleration = 15f;
     [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private float airMovementMultiplier = .5f;
+    [SerializeField] private float groundMovementMultiplier = 10f;
 
     [Header("CAMERA AND ROTATION")]
     [SerializeField] private float camSensi = 400f;
@@ -80,7 +51,6 @@ public class FPSController : MonoBehaviour
     private float currentSpeed = 0f;
 
 
-
     private void TakeInput()
     {
         xMoveInput = Input.GetAxisRaw(Horizontal);
@@ -89,14 +59,19 @@ public class FPSController : MonoBehaviour
         xMouseInput = Input.GetAxisRaw(MouseX);
         yMouseInput = Input.GetAxisRaw(MouseY);
 
+        isJumpPressed = allowJumpButtonHold ? Input.GetKey(jumpKey) : Input.GetKeyDown(jumpKey);
+
+        sprintKeyPressed = Input.GetKey(sprintKey);
+
         moveDir = transform.forward * zMoveInput + transform.right * xMoveInput;
         moveDir = moveDir.normalized;
     }
 
-    private void Move()
+    private void Move(float movementMultiplier = 1)
     { 
         currentSpeed = IsSprinting ? sprintSpeed : walkSpeed;
-        rb.AddForce(moveDir * currentSpeed * 10f, ForceMode.Acceleration);
+        rb.AddForce(moveDir * currentSpeed * groundMovementMultiplier * movementMultiplier, ForceMode.Acceleration);
+        Debug.Log(rb.velocity + " and the current speed is: " +currentSpeed);
     }
 
     private void AccelerateSpeed()
@@ -137,4 +112,114 @@ public class FPSController : MonoBehaviour
        
     }
 
+    #endregion
+
+    #region Jump and gravity
+    [Header("JUMPING")]
+    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private bool allowJumpButtonHold = false;
+    [SerializeField] private float jumpCooldown = .5f;
+
+    private bool isJumpPressed;
+    private bool readyToJump = true;
+
+    [Header("GROUND CHECK")]
+    [SerializeField] private float groundCheckDistance = .2f;
+    [SerializeField] private LayerMask groundLayer;
+    private float playerHieght;
+
+    [Header("Gravity")]
+    [SerializeField] private float gravityForce = 10f;
+    [SerializeField] private float maxFallingSpeed = 10f;
+
+    private void HandleGravity()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.AddForce(-transform.up * gravityForce * Time.deltaTime,ForceMode.Acceleration);
+        }
+
+        rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -maxFallingSpeed, maxFallingSpeed), rb.velocity.z);
+    }
+    
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        var res = Physics.Raycast(transform.position, Vector3.down, playerHieght * .5f + groundCheckDistance, groundLayer);
+
+        return res; 
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void HandleJump()
+    {
+        readyToJump = false;
+
+        Jump();
+        Invoke(nameof(ResetJump), jumpCooldown);
+    }
+
+    private bool ShouldJump()
+    {
+        return isJumpPressed && readyToJump && IsGrounded();
+    }
+
+    #endregion
+
+    #region Unity Methods
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+        capCollider = GetComponentInChildren<CapsuleCollider>();
+        rb.freezeRotation = true;
+    }
+
+    private void Start()
+    {
+        playerHieght = capCollider.bounds.size.y;
+    }
+
+
+    private void Update()
+    {
+        if (!isActive)
+            return;
+
+        TakeInput();
+        RotatePlayer();
+
+        if (ShouldJump())
+            HandleJump();
+        HandleGravity();
+        SpeedControl();
+        AccelerateSpeed();
+       // Debug.Log(rb.velocity);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsGrounded() && canWalk || canSprint)
+             Move();
+
+        else if(!IsGrounded() && canAirMove)
+        {
+            Move(airMovementMultiplier);
+        }
+
+    }
+
+    #endregion
 }
